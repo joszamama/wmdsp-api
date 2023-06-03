1e53c9263cc01c58a9f0977da5ebbbb2194a66f4
joszamama <joszamama@gmail.com>
2023-06-04 00:40:49 +0200

    feat: add home page

--- src/main/java/com/upo/wmdsp/WMdspApplication.java
+++ src/main/java/com/upo/wmdsp/WMdspApplication.java
@@ -3,6 +3,7 @@ package com.upo.wmdsp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class WMdspApplication {
@@ -10,5 +11,10 @@ public class WMdspApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WMdspApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String home() {
+        return "index";
+    }
 
 }
