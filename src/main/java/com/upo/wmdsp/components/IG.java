144aa89357b0db7f78041bf5718b73fa26f74931
joszamama <joszamama@gmail.com>
2023-06-26 10:42:39 +0200

    feat: add convergency computer

--- src/main/java/com/upo/wmdsp/components/IG.java
+++ src/main/java/com/upo/wmdsp/components/IG.java
@@ -88,6 +88,59 @@ public class IG {
                 0, insertionMethod, destructionMethod, reconstructionMethod, runtime, solution.size(), solution);
     }
 
+    public Result runConvergency() {
+        Graph graph = new Graph(filename, K_WEIGHT);
+
+        List<Integer> sizers = new ArrayList<Integer>();
+        List<Double> timers = new ArrayList<Double>();
+        
+        long startTime = System.currentTimeMillis();
+
+        Set<Integer> startingSolution = Insertion.getInitialSolution(graph, insertionMethod);
+        Set<Integer> currentBestSolution = new HashSet<>(startingSolution);
+
+        sizers.add(currentBestSolution.size());
+        timers.add((double) (System.currentTimeMillis() - startTime) / 1000);
+
+        int i = 0;
+
+    
+        while (i < MAX_ITERATIONS_WITHOUT_IMPROVEMENT) {
+            Set<Integer> nextSolution = new HashSet<>(currentBestSolution);
+            Set<Integer> unfeasableSolution = Destruction.getUnfeasibleSolution(graph, nextSolution,
+                    destructionMethod, REMOVE_VERTICES_PERCENTAGE, MINUTES_TIMEOUT, System.currentTimeMillis());
+            Set<Integer> feasibleSolution = Reconstruction.getFeasibleSolution(graph, unfeasableSolution,
+                    reconstructionMethod, MINUTES_TIMEOUT, System.currentTimeMillis());
+
+            if (feasibleSolution.size() < currentBestSolution.size()) {
+                sizers.add(feasibleSolution.size());
+                timers.add((double) (System.currentTimeMillis() - startTime) / 1000);
+                currentBestSolution = new HashSet<>(feasibleSolution);
+                i = 0;
+            } else {
+                i++;
+            }
+
+            if (System.currentTimeMillis() - startTime > MINUTES_TIMEOUT * 60 * 1000) {
+                System.out.println("Global Timeout reached");
+                break;
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        sizers.add(currentBestSolution.size());
+        timers.add((double) (System.currentTimeMillis() - startTime) / 1000);
+
+        System.out.println("Sizers: " + sizers);
+        System.out.println("Timers: " + timers);
+        this.runtime = (endTime - startTime);
+        this.solution = currentBestSolution;
+
+        return new Result(filename, graph.getNumVertices(), graph.getNumEdges(), K_WEIGHT, MAX_ITERATIONS_WITHOUT_IMPROVEMENT, REMOVE_VERTICES_PERCENTAGE,
+                0, insertionMethod, destructionMethod, reconstructionMethod, runtime, solution.size(), solution);
+    }
+
     public Result runWheel() {
         Graph graph = new Graph(filename, K_WEIGHT);
 
@@ -165,13 +218,13 @@ public class IG {
         double REMOVE_VERTICES_PERCENTAGE = 0.2;
         int MAX_ITERATIONS_WITHOUT_IMPROVEMENT = 50;
 
-        InsertionMethod insertionMethod = InsertionMethod.COMPLETE_BASED_INSERTION;
+        InsertionMethod insertionMethod = InsertionMethod.RANDOM_BASED_INSERTION;
         DestructionMethod destructionMethod = DestructionMethod.RANDOM_BASED_DESTRUCTION;
-        ReconstructionMethod reconstructionMethod = ReconstructionMethod.COMPLETE_BASED_RECONSTRUCTION;
+        ReconstructionMethod reconstructionMethod = ReconstructionMethod.RANDOM_BASED_RECONSTRUCTION;
 
         IG ig = new IG(file, K, MAX_ITERATIONS_WITHOUT_IMPROVEMENT, REMOVE_VERTICES_PERCENTAGE, 32,
                 insertionMethod, destructionMethod, reconstructionMethod);
 
-        System.out.println(ig.runGreedy().greedyToString());
+        System.out.println(ig.runConvergency());
     }
 }
